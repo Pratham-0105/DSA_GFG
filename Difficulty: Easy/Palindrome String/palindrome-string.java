c3865b89ee118e5e5eddaadae82e0f15072f8ee0
Prathmesh Ahire <143708266+Pratham-0105@users.noreply.github.com>
2026-07-05 00:08:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        int left = 0;
+        int right = s.length()-1;
+        s = s.toLowerCase();
+
+        while(left<right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            else{
+                if(s.charAt(left) != s.charAt(right)){
+                    return false;
+                }
+                else{
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return true;
+    }
+}
